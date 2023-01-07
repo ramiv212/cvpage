574938136cb129c2725dd2847f1eaefd386c3b1a
ramiv212 <ramiv212@hotmail.com>
2023-01-06 20:20:27 -0600

    removed .env

--- app.py
+++ app.py
@@ -1,19 +1,60 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import json
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+recaptcha = os.environ['RECAPTCHA']
+
+HOST = 'smtp.live.com'
+
+import smtplib
+
+def send_email(subject, to_addr, from_addr, body_text):
+    """
+    Send an email
+    """
+    BODY = "\r\n".join((
+            "From: %s" % from_addr,
+            "To: %s" % to_addr,
+            "Subject: %s" % subject ,
+            "",
+            body_text
+            ))
+    server = smtplib.SMTP(HOST)
+    server.sendmail(from_addr, [to_addr], BODY)
+    server.quit()
+
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html',recaptcha=recaptcha)
 
+
+@app.route("/api/contact", methods=['POST'])
+def send_email():
+    print(request.json['name'])
+
+    json_response = {}
+
+    if not request.json['name']:
+        json_response['name_validation'] = False
+    else:
+        json_response['name_validation'] = True
+
+    if not request.json['email']:
+        json_response['email_validation'] = False
+    else:
+        json_response['email_validation'] = True
+
+    if not request.json['message']:
+        json_response['message_validation'] = False
+    else:
+        json_response['message_validation'] = True
+
+    return json.dumps(json_response)
 
 PORT = os.environ["PORT"]
 app.run(debug=True,host="0.0.0.0", port=PORT)
-
-
-# TODO
-# get my age to auto update on the page. Using jinya and python.
---
+++ static/scripts.js
@@ -0,0 +1,29 @@
+console.log('why are you looking at my console ( ͡° ͜ʖ ͡°)')
+
+function emailValidation(response) {
+
+    if (response && response.name_validation && response.email_validation && response.message_validation) {
+        document.getElementById('contact-form-container').style.display = 'none';
+        document.getElementById('contact-success-message').style.display = 'flex';
+        return}
+    
+    if (response && !response.name_validation) document.getElementById('name-validation').style.display = 'block'
+    if (response && !response.email_validation) document.getElementById('email-validation').style.display = 'block'
+    if (response && !response.message_validation) document.getElementById('message-validation').style.display = 'block'
+}
+
+function sendEmail(body) {
+    fetch('/api/contact', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify(body),
+    })
+    .then((response) => response.json())
+    .then((data) =>{
+        console.log('success!!',data);
+        emailValidation(data)
+    })
+}
